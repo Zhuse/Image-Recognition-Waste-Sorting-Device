5c7b82e8fdc26fdf5edce413a2b2bf3656e966ff
Jeff Yang <jeffjieyang@gmail.com>
2018-03-15 10:16:08 -0700

    update package.json

--- Server/server.js
+++ Server/server.js
@@ -3,3 +3,17 @@ const app = express();
 var bodyParser = require('body-parser');
 
 var port = 3000;
+
+app.use(bodyParser.json());
+
+app.use(express.static('./public'));  // this sends clientside files
+
+
+
+// starting server
+app.listen(port, function(){
+    console.log("Server listening on port " + port);
+});
+
+app.post('/recogniton', function(request, response){
+});
