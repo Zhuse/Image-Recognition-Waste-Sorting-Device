5f8fae2cd3b03c5d30123df696604e066cf8e765
yaoharry <harryyao13@gmail.com>
2018-03-17 19:32:52 +0000

    Sonar trigger finished

--- Hardware/sonar.py
+++ Hardware/sonar.py
@@ -1,35 +1,41 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM) #BOARD??
 
-#Specify Pins, changle later
-TRIG = 23
-ECHO = 24
 
-#Distance measuring
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
-GPIO.output(TRIG, False)
 
-#Sensor Settling
-time.sleep(2)
+def getSonarDistance():
+    GPIO.setmode(GPIO.BCM) #BOARD??
 
-#Sensor pulse
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+    #Specify Pins, changle later
+    TRIG = 18
+    ECHO = 24
 
-#Listen to echo pin
-while GPIO.input(ECHO)==0
-pulse_start = time.time()
+    #Distance measuring
+    GPIO.setup(TRIG, GPIO.OUT)
+    GPIO.setup(ECHO, GPIO.IN)
+    GPIO.output(TRIG, False)
 
-#Signal is recieved
-while GPIO.input(ECHO)==1
-pulse_end = time.time()
+    #Sensor Settling
+    time.sleep(2)
+    #Sensor pulse
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
 
-#Calculate and print distance
-pulse_duration = pulse_end - pulse_start
-distance = round(distance, 2)
-print "Distance",distance,"cm"
+    #Listen to echo pin
+    while GPIO.input(ECHO)==0:
+        pulse_start = time.time()
 
-GPIO.cleanup()
+    #Signal is recieved
+    while GPIO.input(ECHO)==1:
+        pulse_end = time.time()
+
+    #Calculate and print distance
+    pulse_duration = pulse_end - pulse_start
+    distance = (pulse_duration * 34300) /2
+    print ("Distance",distance,"cm")
+
+    return distance
+
+def GPIOCleanup():
+    GPIO.cleanup()
--- Hardware/triggerPi.py
+++ Hardware/triggerPi.py
@@ -1,6 +1,16 @@
 from take_photo import *
+from sonar import *
 import base64
 
-capturePic()
-convertToBase64("test_photo.jpg")
+objectDistance = 9999999
 
+#setupGPIO()
+
+while objectDistance > 50:
+    objectDistance = getSonarDistance()
+
+if (objectDistance < 50):
+    capturePic()
+    #convertToBase64("test_photo.jpg")
+
+GPIOCleanup()
