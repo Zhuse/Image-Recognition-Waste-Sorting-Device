b145dbb85d8d4e86bad2f1f4031d1fb1b679b6ee
Steven Zhu <stevenseraser@gmail.com>
2018-03-16 18:23:40 -0700

    Added AWS dependencies

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
+var AWS = require('aws-sdk');
 
 var port = 3000;
 
@@ -8,7 +9,7 @@ app.use(bodyParser.json());
 
 app.use(express.static('./public'));  // this sends clientside files
 
-
+var rekognition = new AWS.Rekognition();
 
 // starting server
 app.listen(port, function(){
