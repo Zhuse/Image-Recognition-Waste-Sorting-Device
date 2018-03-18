a22a3420d8ff801eb6fd804f0add20b75ede3197
William Gu <williamgu2011@hotmail.com>
2018-03-18 14:50:28 -0700

    Image recognition functions working

--- Server/image_recognition.js
+++ Server/image_recognition.js
@@ -6,75 +6,70 @@ AWS.config.loadFromPath('config.json');
 var recog = new AWS.Rekognition();
 var s3 = new AWS.S3(); //amazon cloud storage service
 
-// call S3 to retrieve upload file to specified bucket
-/*
-var uploadParams = {Bucket: 'cpen291-g15', Key: '', Body: ''}; //Bucket name should be "cpen291-g15"
-var file = process.argv[2];
-
-var fileStream = fs.createReadStream(file);
-fileStream.on('error', function(err) {
-  console.log('File Error', err);
+//Enums for garbage types
+const GARBAGE_TYPE = Object.freeze({
+    GARBAGE: "garbage",
+    RECYCLING: "recycling",
+    COMPOST: "compost"
 });
-uploadParams.Body = fileStream;
-uploadParams.Key = path.basename(file);
 
-s3.upload (uploadParams, function (err, data) {
-  if (err) {
-    console.log("Error", err);
-  } if (data) {
-    console.log("Upload Success", data.Location);
-  }
-});*/
-let params = {};
-
-let content = fs.readFileSync('test_pictures/doge.jpg');
-params.Image = {
-    Bytes: content
+//Possible tags and their garbage classification
+const GARBAGE_TAGS = {
+    "Produce": GARBAGE_TYPE.COMPOST,
+    "Food": GARBAGE_TYPE.COMPOST,
+    "Plant": GARBAGE_TYPE.COMPOST,
+	"Bottle": GARBAGE_TYPE.RECYCLING,
+	"Beverage": GARBAGE_TYPE.RECYCLING
 };
 
-let faces = new Promise((resolve, reject) => {
-    recog.detectFaces(params, function(err, data) {
-        if (err) reject(err);
-        console.log(data);
-    });
-});
-let labels = new Promise((resolve, reject) => {
-    recog.detectLabels(params, function(err, data) {
-        if (err) reject(err);
-        console.log(data);
-    });
-});
-let modlabels = new Promise((resolve, reject) => {
-    recog.detectModerationLabels(params, function(err, data) {
-        if (err) reject(err);
-        console.log(data);
-    });
-});
-let celebs = new Promise((resolve, reject) => {
-    recog.recognizeCelebrities(params, function(err, data) {
-        if (err) reject(err);
-        console.log(data);
-    });
-});
+/**
+ * Given an image, detects the most likely type of garbage that it is
+ * and returns that type as a string
+ * @param  {[String]} imagePath location of image to detect
+ * @return {[Promise]} promise that will resolve to a GARBAGE_TYPE
+ */
+function detectLabels(imagePath) {
+    var params = {};
+    var content = fs.readFileSync(imagePath);
+    params.Image = {
+        Bytes: content
+    };
+    params.MinConfidence = 70;
 
-/*
-return new Promise((resolve, reject) => {
-
-    console.log('Attempting Amazon recog');
-    Promise.all([faces, labels, modlabels, celebs]).then(values => {
-        let faces = values[0];
-        let labels = values[1];
-        let modlabels = values[2];
-        let celebs = values[3];
-        let result = {
-            faces: faces,
-            labels: labels,
-            modlabels: modlabels,
-            celebs: celebs
-        }
-        resolve({
-            "amazon": result
+    return new Promise((resolve, reject) => {
+        recog.detectLabels(params, function(err, data) {
+            if (err) reject(err);
+            resolve(detectGarbage(data));
         });
     });
+}
+
+/**
+ * A helper function for detectLabels, analyzes the labels given in imageData
+ * and returns a string for the most likely type of garbage it is
+ * @param  {[Object]} imageData object containing image labels
+ * @return {[GARBAGE_TYPE]} type of garbage detected from image labels (string object)
+ */
+function detectGarbage(imageData) {
+	console.log(imageData.Labels);
+    for (var i = 0; i < imageData.Labels.length; i++) {
+        for (tag in GARBAGE_TAGS) {
+            if (imageData.Labels[i].Name == tag) { //If we find a major identifying garbage type tag, immediately return the prediction
+                return GARBAGE_TAGS[tag];
+            }
+        }
+    }
+    return GARBAGE_TYPE.GARBAGE; //If we can't figure out what it is, classify it as garbage
+}
+
+/* TESTING */
+const water_bottle = 'test_pictures/plastic_bottle.jpg';
+const banana_peel = 'test_pictures/banana_peel.jpg';
+
+/******EXAMPLE USAGE OF PROMISE******/
+detectLabels(water_bottle).then(function(res) {
+    console.log(res) //res holds garbage_type info
 });
-*/
+detectLabels(banana_peel).then(function(res) {
+    console.log(res)
+});
