b1886029d30d83e2f31c95d69da8a51eb47cff63
Darming Zhao <darmingzhao@gmail.com>
2018-03-20 10:55:39 -0700

    added led to main code

--- Hardware/led.py
+++ Hardware/led.py
@@ -1,8 +1,6 @@
 from gpiozero import LED
 from time import sleep
 
-#initialize LED
-led1 = LED(21)
 ledRed = LED(11)
 ledGreen = LED(13)
 ledBlue = LED(15)
@@ -10,18 +8,6 @@ ledBlue = LED(15)
 def greenOn():
     ledGreen.off()
 
-def greenOff()
+def greenOff():
     ledGreen.on()
-
-def ledFlash():
-    #turn LEDs on then off in a five second interval 
-    led1.off()
-    sleep(2)
-    led1.on()
-
-def ledSignal(distance):
-    if distance < 3: #if bin is full, turn LED on
-        led1.off()
-    else:
-        led1.on() #turn LED off otherwise
     
