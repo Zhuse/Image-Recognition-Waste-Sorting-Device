922410e9dce5fce408a3ffabd20aa914e8904fa3
Pairallax <30737313+Pairallax@users.noreply.github.com>
2018-03-20 11:03:34 -0700

    App working with sample JSON server

--- App/TrashViewer/Screens/GarbageScreen.js
+++ App/TrashViewer/Screens/GarbageScreen.js
@@ -1,4 +1,98 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, FlatList, Image, ActivityIndicator} from 'react-native'
+
+export default class GarbageScreen extends Component {
+    constructor() {
+        super();
+        this.state = {
+            dataSource: [],
+            isLoading: true
+        }
+    }
+
+    /*TODO change item.fields below*/
+        renderItem = ({item}) => {
+            return (
+                <View style={{flex: 1, flexDirection: 'row', marginBottom: 2}}>
+                    <Image style={{width: 80, height: 80, margin: 3}}
+                           source={{uri: item.picture.thumbnail}}/>
+                    <View style={{flex:1, justifyContent: 'center', marginLeft: 3}}>
+                        <Text style={{fontSize: 18, color: 'black', marginBottom: 10}}>
+                            {`${item.name.first} ${item.registered}`}
+                        </Text>
+                        <Text style={{fontSize: 14, color: 'green'}}>
+                            {`${item.dob}`}
+                        </Text>
+                    </View>
+                </View>
+            )
+        };
+
+    renderSeparator = () => {
+            return (
+                <View style={{height: 1, width: '100%', backgroundColor: 'blue'}}>
+                </View>
+            )
+        };
+
+    componentDidMount() {
+        const url = "https://randomuser.me/api?results=500"; /*TODO change this */
+        fetch(url)
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    dataSource: responseJson.results, /*TODO change this */
+                    isLoading: false
+                })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    render() {
+        return (
+            this.state.isLoading
+                ?
+                <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                    <ActivityIndicator size="large" color="#330066" animating/>
+                </View>
+                :
+                <View style={styles.container}>
+                    <FlatList
+                        data={this.state.dataSource}
+                        renderItem={this.renderItem}
+                        keyExtractor={(item, index) => index}
+                        ItemSeparatorComponent={this.renderSeparator}
+                    />
+                </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 25,
+        backgroundColor: '#bff6ff'
+    },
+    item: {
+        padding: 10,
+        fontSize: 15,
+        height: 40,
+    },
+});
+
+/*const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF'
+    }
+});*/
+
+
+//WORKING BUT OLD CODE
+/*import React, { Component } from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 
 export default class App extends Component {
@@ -44,130 +138,4 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
     },
-});
-
-/*    render() {
-        return (
-            <View style={styles.container}>
-                <FlatList
-                    data={this.state.data}
-                    keyExtractor={(x, i) => i}
-                    renderItem={({ item }) =>
-                        <View style={{flex: 1, flexDirection: 'row', marginBottom: 3}}>
-                            {/!*             <Image style={{width: 100, height: 100, margin: 5}}
-                    //source={{uri: item.image}}/> //TODO change this
-                       source={{uri: item.picture}}/> //TODO change this*!/}
-                            <View style={{flex: 1, justifyContent: 'center', marginLeft: 5}}>
-                                <Text style={{fontSize: 18, color: 'green', marginBottom: 15}}>
-                                    /!*{item.name}*!/ //TODO change this
-                                    {`${item.name.first} ${item.name.last}`} //TODO change this
-                                </Text>
-                                <Text style={{fontSize: 18, color: 'green'}}>
-                                    /!*{item.date}*!/ //TODO change this
-                                    {`${item.name.first} ${item.name.last}`} //TODO change this
-                                </Text>
-                            </View>
-                        </View>}
-                />
-            </View>
-        );
-    }*/
-
-/*const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 22
-    },
-    item: {
-        padding: 10,
-        fontSize: 18,
-        height: 44,
-    },
-})*/
-
-/*
-import React, {Component} from 'react';
-import {StyleSheet, Text, View, FlatList, Image, ActivityIndicator} from 'react-native'
-
-export default class GarbageScreen extends Component {
-    constructor() {
-        super();
-        this.state = {
-            dataSource: [],
-            isLoading: true
-        }
-    }
-
-
-
-    renderItem = ({item}) => {
-        return (
-            <View>
-                <Image style={{width: 100, height: 100}}
-                    //source={{uri: item.image}}/> //TODO change this
-                       source={{uri: item.picture}}/> //TODO change this
-                <View>
-                    <Text>
-                        /!*{item.name}*!/ //TODO change this
-                        {item.name} //TODO change this
-                    </Text>
-                    <Text>
-                        /!*{item.date}*!/ //TODO change this
-                        {item.company} //TODO change this
-                    </Text>
-                </View>
-            </View>
-        )
-    };
-/!*
-    renderSeparator = () => {
-        return (
-            <View>
-                style={{height: 1, width: '100%', backgroundColor: 'black'}}>
-            </View>
-        )
-    };*!/
-
-    componentDidMount() {
-        //const url = 'SERVER URL'; //TODO change this
-        const url = 'http://www.json-generator.com/api/json/get/bVttMJAQeq?indent=2'; //TODO change this
-        fetch(url)
-            .then((response) => response.json())
-            .then((responseJson) => {
-                this.setState({
-                    //dataSource: responseJson.DATAFIELDOFINTEREST, //TODO change this
-                    dataSource: responseJson, //TODO change this
-                    isLoading: false
-                })
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-    }
-
-    render() {
-        return (
-            /!*this.state.isLoading
-                ?
-                <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                    <ActivityIndicator size="large" color="#330066" animating/>
-                </View>
-                :*!/
-                <View style={styles.container}>
-                    <FlatList
-                        data={this.state.dataSource}
-                        renderItem={this.renderItem}
-                        keyExtractor={(item, index) => index}
-                        //ItemSeparatorComponent={this.renderSeparator}
-                    />
-                </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#F5FCFF'
-    }
 });*/
