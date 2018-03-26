da8e44afc637236e444283f3a5719a8bd72634ed
William Gu <williamgu2011@hotmail.com>
2018-03-25 18:25:51 -0700

    Added js file for all routes

---
+++ Server/routes.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const recognitionController = require('./controllers/image-recognition-controller.js'); //Import recog controller
+
+/**
+ * Test request handler
+ */
+router.get('/test', function(request, response) {
+    console.log("test executing");
+    recognitionController.test().then((category) => {
+        response.send(category); //res holds garbage_type info
+    })
+});
+
+/**
+ * Testdb request handler
+ */
+router.get('/testdb', function(request, response){
+  console.log("database test executing");
+  //recognitionController.test;
+  var results = databaseController.updateDatabase(1, 20, 30, 40);
+  response.json({"results": results});
+});
+
+
+// TODO what format will json be
+// TODO error handling
+/**
+ * Post request handler for recognition detection requests
+ * @param  {[type]} request  [description]
+ * @param  {[type]} response [description]
+ * @return {[type]}          [description]
+ */
+router.post('/recognition', function(request, response) {
+    var enc = new Buffer(request.body.base64, 'base64');
+	//var enc = request.query.base64;
+    recognitionController.recognition(enc).then((category) => {
+        response.json({
+            "category": category
+        });
+    });
+});
+
+module.exports = router;
--- Server/server.js
+++ Server/server.js
@@ -17,6 +17,12 @@ module.exports.AWS = AWS;
 module.exports.recog = recog;
 module.exports.fs = fs;
 module.exports.db = db;
+module.exports = {
+	AWS,
+	recog,
+	fs,
+	db
+};
 
 const recognitionController = require('./controllers/image-recognition-controller.js'); //Import recog controller
 
@@ -24,48 +30,9 @@ const port = 3000; //Server port
 
 app.use(express.json()); //to support JSON-encoded bodies
 app.use(express.static('./public')); // this sends clientside files
-
+app.use(require('./routes.js')); //Use all routes defined in routes.js
 
 // starting server
 app.listen(port, function() {
     console.log("Server listening on port " + port);
-});
-
-/**
- * Test request handler
- */
-app.get('/test', function(request, response) {
-    console.log("test executing");
-    recognitionController.test().then((category) => {
-        response.send(category); //res holds garbage_type info
-    })
-});
-
-/**
- * Testdb request handler
- */
-app.get('/testdb', function(request, response){
-  console.log("database test executing");
-  //recognitionController.test;
-  var results = databaseController.updateDatabase(1, 20, 30, 40);
-  response.json({"results": results});
-});
-
-
-// TODO what format will json be
-// TODO error handling
-/**
- * Post request handler for recognition detection requests
- * @param  {[type]} request  [description]
- * @param  {[type]} response [description]
- * @return {[type]}          [description]
- */
-app.post('/recognition', function(request, response) {
-    var enc = new Buffer(request.body.base64, 'base64');
-	//var enc = request.query.base64;
-    recognitionController.recognition(enc).then((category) => {
-        response.json({
-            "category": category
-        });
-    });
 });
