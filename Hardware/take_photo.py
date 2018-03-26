524d687695cb1e9c318070c8aa7b665448bdfb54
yaoharry <harryyao13@gmail.com>
2018-03-26 04:49:38 +0000

    working hardware

--- Hardware/take_photo.py
+++ Hardware/take_photo.py
@@ -17,23 +17,23 @@ def capturePic():
 def convertToBase64(fileName):
     with open(fileName, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
-        print (encoded_string)
         return encoded_string
 
 def postServer(base64Image):
-    connection = http.client.HTTPSConnection('api.github.com')
+    connection = http.client.HTTPSConnection('34.218.219.101:3000')
     headers = {'Content-type' : 'application/json'}
 
     jsonImage = {'base64Image': base64Image}
     json_foo = json.dumps(jsonImage)
 
-    connection.request('POST', '/markdown', json_foo, headers)
+    connection.request('POST', '/recognition', json_foo, headers)
 
     response = connection.getresponse()
     print(response.read().decode())
 
 def altPostServer(base64Image):
-    API_ENDPOINT = "34.218.219.101:3000"
-    r = requests.get(API_ENDPOINT, data = {'base_64': base64Image})
+    payload = {'base64': str(base64Image)}
+    r = requests.post('http://34.218.219.101:3000/recognition', json=payload)
+    print (r.text)
     return r
 
--- Hardware/triggerPi.py
+++ Hardware/triggerPi.py
@@ -2,14 +2,15 @@ from take_photo import *
 from sonar import *
 from servoMotor import *
 from led import *
-#from uploadImage import *
-
+#froom uploadImage import *
+import json
+import requests
+import time
 import base64
 
 objectDistance = 9999999
 
 #setupGPIO()
-
 while objectDistance > 50:
     objectDistance = getSonarDistance()
 
@@ -18,7 +19,9 @@ if (objectDistance < 50):
     capturePic()
     greenOff()
     base64String = convertToBase64("test_photo.jpg")
-    requestRet = altPostServer(base64String)
+    time.sleep(1)
+    altPostServer(base64String)  
+
     binNumber = requestRet[open_bin]
     openBin(binNumber)
 
