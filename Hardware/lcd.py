105dccd694d85f86b4f5339656445807b30747c8
Darming Zhao <darmingzhao@gmail.com>
2018-03-28 18:06:27 -0700

    added comments for lcd code

--- Hardware/lcd.py
+++ Hardware/lcd.py
@@ -4,10 +4,12 @@
 
 #Display is done in 4-bit mode (in case more hardware is added to the Pi later)
 
+#intialize lcd
 from RPLCD import CharLCD
 import time
 lcd = CharLCD(cols = 16, rows = 2, pin_rs = 37, pin_e = 35, pinds_data = [33, 31, 29, 23])
 
+#update LCD message depending on which bin is open
 def updateLCD(char bin) {
     if bin == 'c'
         LCD.write_string("Place Waste Inside Compost")
@@ -22,8 +24,10 @@ def updateLCD(char bin) {
     LCD.clear()
 }
 
+#Default message on the LCD
 def standbyLCD()
     LCD.write_string("Place Waste Next to Webcam")
 
+#Clear message on LCD
 def clearLCD()
     LCD.clear()
