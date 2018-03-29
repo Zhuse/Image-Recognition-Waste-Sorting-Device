234d0825d4e0a8ba2349df275573fbb7b5aa4a7d
Darming Zhao <darmingzhao@gmail.com>
2018-03-28 18:08:45 -0700

    updated lcd code

--- Hardware/lcd.py
+++ Hardware/lcd.py
@@ -12,22 +12,22 @@ lcd = CharLCD(cols = 16, rows = 2, pin_rs = 37, pin_e = 35, pinds_data = [33, 31
 #update LCD message depending on which bin is open
 def updateLCD(char bin) {
     if bin == 'c'
-        LCD.write_string("Place Waste Inside Compost")
+        lcd.write_string("Place Waste Inside Compost")
     elif bin == 'g'
-        LCD.write_string("Place Waste Inside Garbage")
+        lcd.write_string("Place Waste Inside Garbage")
     elif bin == 'r'
-        LCD.write_string("Place Waste Inside Recycling")
+        lcd.write_string("Place Waste Inside Recycling")
     else
-        LCD.write_string("Place Waste Next to Webcam")
+        lcd.write_string("Place Waste Next to Webcam")
     
     time.sleep(5)
-    LCD.clear()
+    lcd.clear()
 }
 
 #Default message on the LCD
 def standbyLCD()
-    LCD.write_string("Place Waste Next to Webcam")
+    lcd.write_string("Place Waste Next to Webcam")
 
 #Clear message on LCD
 def clearLCD()
-    LCD.clear()
+    lcd.clear()
