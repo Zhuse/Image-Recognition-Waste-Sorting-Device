99009353078074746cdbfc6851dcef28611d24e7
Pairallax <30737313+Pairallax@users.noreply.github.com>
2018-03-30 21:08:30 -0700

    Adding GET request functionality

--- App/TrashViewer/Screens/GarbageScreen.js
+++ App/TrashViewer/Screens/GarbageScreen.js
@@ -1,60 +1,119 @@
 import React, {Component} from 'react';
 import {insertCommand} from '../Networking/server';
-import {Text, View, Image, TouchableOpacity, StyleSheet} from 'react-native'
+import {Text, View, Image, TouchableOpacity, StyleSheet, FlatList, ActivityIndicator} from 'react-native'
 import garbageIcon from '../images/garbage.png';
 
 export default class GarbageScreen extends Component {
     constructor() {
         super();
         this.state = {
-            timesOpened: 0
+            timesOpened: 0,
+            dataSource: [],
+            isLoading: true
         }
     }
 
     render() {
         return (
-            <View
-                style={{
-                    flex: 1,
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }}>
+            this.state.isLoading
+                ?
+                <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                    <ActivityIndicator size="large" color="#330066" animating/>
+                </View>
+                :
+                <View>
+                    <View
+                        style={{
+                            flex: 1,
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}>
 
-                <TouchableOpacity
-                    style={styles.icon}
+                        <TouchableOpacity
+                            style={styles.icon}
 
-                    onPress={() => {
-                        const newCommand = {
-                            openGarbage: true,
-                            openCompost: false,
-                            openRecycling: false
-                        };
+                            onPress={() => {
+                                const newCommand = {
+                                    openGarbage: true,
+                                    openCompost: false,
+                                    openRecycling: false
+                                };
 
-                        insertCommand(newCommand).then((result) => {
-                            if (result === 'ok') {
-                                this.state.timesOpened++;
-                            }
-                            else {
-                                alert("could not open bin");
-                            }
-                        });
-                    }}>
+                                insertCommand(newCommand).then((result) => {
+                                    if (result === 'ok') {
+                                        this.state.timesOpened++;
+                                    }
+                                    else {
+                                        alert("could not open bin");
+                                    }
+                                });
+                            }}>
 
-                    <Image
-                        source={garbageIcon}
-                        style={styles.icon}
-                    />
+                            <Image
+                                source={garbageIcon}
+                                style={styles.icon}
+                            />
 
-                </TouchableOpacity>
+                        </TouchableOpacity>
 
-                <Text
-                    style={styles.font}>
-                    Times Opened: {this.state.timesOpened.toString()}
-                </Text>
+                        <Text
+                            style={styles.font}>
+                            Times Opened: {this.state.timesOpened.toString()}
+                        </Text>
+                    </View>
 
-            </View>
+                    <View style={styles.container}>
+                        <FlatList
+                            data={this.state.dataSource}
+                            renderItem={this.renderItem}
+                            keyExtractor={(item, index) => index}
+                            ItemSeparatorComponent={this.renderSeparator}
+                        />
+                    </View>
+
+                </View>
+
         );
+    }
+
+    renderItem = ({item}) => {
+        return (
+            <View style={{flex: 1, flexDirection: 'row', marginBottom: 2}}>
+                <Image style={{width: 80, height: 80, margin: 3}}
+                       source={{uri: item.picture.thumbnail}}/>
+                <View style={{flex: 1, justifyContent: 'center', marginLeft: 3}}>
+                    <Text style={{fontSize: 18, color: 'black', marginBottom: 10}}>
+                        {`${item.name.first} ${item.registered}`}
+                    </Text>
+                    <Text style={{fontSize: 14, color: 'green'}}>
+                        {`${item.dob}`}
+                    </Text>
+                </View>
+            </View>
+        )
+    };
+
+    renderSeparator = () => {
+        return (
+            <View style={{height: 1, width: '100%', backgroundColor: 'blue'}}>
+            </View>
+        )
+    };
+
+    componentDidMount() {
+        const url = "https://randomuser.me/api?results=500";
+        fetch(url)
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    dataSource: responseJson.results,
+                    isLoading: false
+                })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
 }
 
@@ -68,83 +127,7 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
         marginTop: 30
-    }
-});
-
-//TODO code for GET requests below
-/*/!*TODO change item.fields below*!/
-constructor()
-{
-    super();
-    this.state = {
-        dataSource: [],
-        isLoading: true
-    }
-}
-
-renderItem = ({item}) => {
-    return (
-        <View style={{flex: 1, flexDirection: 'row', marginBottom: 2}}>
-            <Image style={{width: 80, height: 80, margin: 3}}
-                   source={{uri: item.picture.thumbnail}}/>
-            <View style={{flex: 1, justifyContent: 'center', marginLeft: 3}}>
-                <Text style={{fontSize: 18, color: 'black', marginBottom: 10}}>
-                    {`${item.name.first} ${item.registered}`}
-                </Text>
-                <Text style={{fontSize: 14, color: 'green'}}>
-                    {`${item.dob}`}
-                </Text>
-            </View>
-        </View>
-    )
-};
-
-renderSeparator = () => {
-    return (
-        <View style={{height: 1, width: '100%', backgroundColor: 'blue'}}>
-        </View>
-    )
-};
-
-componentDidMount()
-{
-    const url = "https://randomuser.me/api?results=500";
-    /!*TODO change this *!/
-    fetch(url)
-        .then((response) => response.json())
-        .then((responseJson) => {
-            this.setState({
-                dataSource: responseJson.results, /!*TODO change this *!/
-                isLoading: false
-            })
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-}
-
-render()
-{
-    return (
-        this.state.isLoading
-            ?
-            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <ActivityIndicator size="large" color="#330066" animating/>
-            </View>
-            :
-            <View style={styles.container}>
-                <FlatList
-                    data={this.state.dataSource}
-                    renderItem={this.renderItem}
-                    keyExtractor={(item, index) => index}
-                    ItemSeparatorComponent={this.renderSeparator}
-                />
-            </View>
-    );
-}
-}
-
-const styles = StyleSheet.create({
+    },
     container: {
         flex: 1,
         paddingTop: 25,
@@ -156,60 +139,3 @@ const styles = StyleSheet.create({
         height: 40,
     },
 });
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#F5FCFF'
-    }
-});*/
-
-
-//WORKING BUT OLD CODE
-/*import React, { Component } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
-
-export default class App extends Component {
-    state = {
-        dataSource: []
-    };
-
-    componentWillMount() {
-        this.fetchData();
-    }
-
-    fetchData = async () => {
-        const serverResponse = await fetch("https://randomuser.me/api?results=500");
-        const JSON = await serverResponse.json();
-        this.setState({ dataSource: JSON.results });
-    };
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <FlatList
-                    data={this.state.dataSource}
-                    keyExtractor={(x, i) => i}
-                    renderItem={({ item }) =>
-                        <Text style = {styles.titleText}>
-                            {`${item.name.first} ${item.registered}`}
-                        </Text>}
-                />
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        marginTop: 15,
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#F5FCFF",
-    },
-    titleText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-});*/
