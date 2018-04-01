ee38ecca0944439872bc02b1751ca457de5bf491
Darming Zhao <darmingzhao@gmail.com>
2018-04-01 16:18:21 -0700

    added comments to led code

--- Hardware/led.py
+++ Hardware/led.py
@@ -1,11 +1,15 @@
 from gpiozero import LED
 from time import sleep
 
+#initialize each color of the RGB led
+#TODO: change pins
 ledRed = LED(11)
 ledGreen = LED(14)
 ledBlue = LED(15)
 
-ledF = LED(21) #initialize each segment of seven segment display
+#initialize each segment of the seven degment led
+#TODO: change pins
+ledF = LED(21)
 ledG = LED(20)
 ledE = LED(16)
 ledD = LED(12)
@@ -14,8 +18,9 @@ ledC = LED(19)
 ledB = LED(13)
 ledA = LED(22)
 
+#turn seven segment led off
 def segOff():   
-    ledF.on() #turn all segments off
+    ledF.on()
     ledG.on()
     ledE.on()
     ledD.on()
@@ -24,16 +29,18 @@ def segOff():
     ledB.on()
     ledA.on()
 
+#display c on seven segment led
 def segC():
-    ledF.off()
-    ledG.on()
+    ledF.on()
+    ledG.off()
     ledE.off()
     ledD.off()
     ledDP.on()
     ledC.on()
     ledB.on()
-    ledA.off()
+    ledA.on()
 
+#display g on seven segment led
 def segG():
     ledF.off()
     ledG.off()
@@ -44,16 +51,18 @@ def segG():
     ledB.off()
     ledA.off()
 
+#display r on seven segment led
 def segR():
-    ledF.off()
-    ledG.on()
+    ledF.on()
+    ledG.off()
     ledE.off()
     ledD.on()
     ledDP.on()
     ledC.on()
     ledB.on()
-    ledA.off()
+    ledA.on()
 
+#update display on seven segment display depending on which bin is open
 def updateSeg(bin):
     if bin == 'compost':
         segC()
@@ -66,9 +75,11 @@ def updateSeg(bin):
     sleep(5)
     segOff()
 
+#turn RGB led on in green
 def greenOn():
     ledGreen.off()
 
+#turn RGB led off in green
 def greenOff():
     ledGreen.on()
     
