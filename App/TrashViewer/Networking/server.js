7eb4cdea203ad79d05c107ae9a9486086383b691
Pairallax <30737313+Pairallax@users.noreply.github.com>
2018-04-01 00:26:57 -0700

    Removed GET functionality since everything is done through POST

--- App/TrashViewer/Networking/server.js
+++ App/TrashViewer/Networking/server.js
@@ -1,6 +1,5 @@
 const apiInsertCommand = 'http://34.218.219.101:3000/setMode'; //TODO change this
 const apigetItemHistory = 'http://34.218.219.101:3000/history';//TODO change this
-const apiFetchCommand = 'http://localhost:3001/fetch_command';
 
 //send POST request to insert new data
 async function insertCommand(command) {
@@ -50,17 +49,6 @@ async function getItemHistory(command) {
     }
 }
 
-//send GET request to fetch data
-async function fetchItemData() {
-    try {
-        let response = await fetch(apiFetchCommand);
-        let responseJson = await response.json();
-        return responseJson.json;
-    } catch (error) {
-        console.error(`Error is : ${error}`);
-    }
-}
 
 export {insertCommand};
 export {getItemHistory};
-export {fetchItemData};
